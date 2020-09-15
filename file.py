4419fb8cc0ea744fc3e9c23e0c82cb4fdd9952dd
jianghs <jianghs_own@163.com>
2020-09-15 23:52:34 +0800

    change encoding

--- file.py
+++ file.py
@@ -6,17 +6,17 @@
 import csv
 
 # 写入文件（覆盖）
-with open('output\\text.txt', 'w', encoding="utf-8") as f:
+with open('output/text.txt', 'w', encoding="utf-8") as f:
     f.write('你好！')
     f.write('\n你好！')
     f.write('\n你好！')
 
 # 写入文件（文件末尾追加）
-with open('output\\text.txt', 'a', encoding="utf-8") as f:
+with open('output/text.txt', 'a', encoding="utf-8") as f:
     f.write('\n你好！')
 
 # 读取文件
-with open("output\\text.txt", "r", encoding="utf-8") as f:
+with open("output/text.txt", "r", encoding="utf-8") as f:
     for line in f.readlines():
         print(line.strip())
 
@@ -25,13 +25,13 @@ data = [{"name": "宝宝", "sex": "女", "age": "18"},
         {"name": "嘻嘻", "sex": "女", "age": "18"},
         {"name": "哈哈", "sex": "女", "age": "18"}]
 
-with open('output\\input.csv', 'w', encoding="GBK") as f:
+with open('output/input.csv', 'w', encoding="utf-8") as f:
     writer = csv.DictWriter(f, fieldnames=["name", "sex", "age"])
     writer.writeheader()
     writer.writerows(data)
 
 # 读取CSV文件
-with open("output\\input.csv", "r", encoding="GBK") as f:
+with open("output/input.csv", "r", encoding="utf-8") as f:
     reader = csv.DictReader(f)
     for line in reader:
         print(line)
