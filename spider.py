ec191ae226d7e30da441921f9f8b6a3ef3a0a10c
jianghs430 <jianghs430@zto.com>
2020-09-21 19:00:38 +0800

    full codes

---
+++ spider.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import re
+import requests
+import csv
+from bs4 import BeautifulSoup
+
+source = requests.get("https://tieba.baidu.com/p/6970589236").content.decode()
+soup = BeautifulSoup(source, "lxml")
+# print(soup)
+# 获取帖子内容
+info = soup.find(class_="p_postlist")
+
+# 获取帖子内容
+answer_list = info.find_all(class_=re.compile("l_post j_l_post l_post_bright*"))
+
+data = []
+for answer in answer_list:
+    name = answer.find(class_=re.compile("p_author_name*"))
+    content = answer.find(class_=re.compile("d_post_content j_d_post_content*"))
+    obj = {"name": name.text, "content": content.text}
+    data.append(obj)
+    # print("作者：%s，内容：%s" % (name.text, content.text))
+
+
+# 写入CSV文件
+with open('output/spider_result.csv', 'w', encoding="utf-8") as f:
+    writer = csv.DictWriter(f, fieldnames=["name", "content"])
+    writer.writeheader()
+    writer.writerows(data)
+
+
